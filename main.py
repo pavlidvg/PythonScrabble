8f6bb85c67d8b6288ad85fdfd56fef3e7f215c52
pavlidvg <74056182+pavlidvg@users.noreply.github.com>
2023-07-07 00:45:05 +0300

    fixed main

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    classes.main()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
